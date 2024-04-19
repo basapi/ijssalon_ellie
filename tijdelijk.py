29ffbf72c986a3e83d6aceda4add4ef2ade56ca7
Bas van Amersfoort <bas.van.amersfoort@exact.com>
2024-04-19 11:55:31 +0200

    bestand orgineel tijdeljk.py

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,5 +1,6 @@
 
 
+
 Prijzen = {
     "Aardbei":3,
     "Vanille":4,
@@ -8,20 +9,24 @@ Prijzen = {
 
 Aanbieding = Prijzen["Aardbei"] * 0.8
 
-
+print(Prijzen)
 
 reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts $ {Aanbieding}")
+print(reclame_tekst)
 
 reclame_tekst2 = reclame_tekst[:63]
+print(reclame_tekst2)
 
-reclame_tekst3 = reclame_tekst2.upper()
+reclame_tekst3 = reclame_tekst2
+print(reclame_tekst3.upper())
 
-reclame_tekst4 = reclame_tekst3.split()
+reclame_tekst4 = reclame_tekst3 
+print(reclame_tekst4.split(" "))
 
-#for el in reclame_tekst4:
-#print(el)
+for el in reclame_tekst4:
+ print(el)
 
-#print(reclame_tekst4.lower())
+print(reclame_tekst4.lower())
 
 for el in reclame_tekst4:
     if len(el) >= 5:
