78538c48553dfdca8452bab276399ee36d5314f7
Bas van Amersfoort <bas.van.amersfoort@exact.com>
2024-04-21 16:01:29 +0200

    functies toegevoegd

---
+++ algemene_functies.py
@@ -0,0 +1,12 @@
+totaal = 2
+a = 2
+
+def mijn_functie():  
+    
+ global a
+
+ a = 2
+
+totaal = totaal * a
+
+print(totaal)
--- tijdelijk.py
+++ tijdelijk.py
@@ -1,30 +1,29 @@
+from helper import decoreer
 
-
-Prijzen = {
+def print_aanbieding():
+ prijzen = {
     "Aardbei":3,
     "Vanille":4,
     "Chocolade":5
 }
 
-Aanbieding = Prijzen["Aardbei"] * 0.8
+ aanbieding = prijzen["Aardbei"] * 0.8
 
+ reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts $ {aanbieding}")
 
+ reclame_tekst2 = reclame_tekst[:63]
 
-reclame_tekst = (f"Vandaag in de aanbieding: vanille-ijs, 1 liter - slechts $ {Aanbieding}")
+ reclame_tekst3 = reclame_tekst2.upper()
 
-reclame_tekst2 = reclame_tekst[:63]
+ reclame_tekst4 = reclame_tekst3.split()
 
-reclame_tekst3 = reclame_tekst2.upper()
-
-reclame_tekst4 = reclame_tekst3.split()
-
-#for el in reclame_tekst4:
-#print(el)
-
-#print(reclame_tekst4.lower())
-
-for el in reclame_tekst4:
+ for el in reclame_tekst4:
     if len(el) >= 5:
         print(el.upper())
     else:
         print(el.lower())
+
+decoreer("Aanbieding")
+        
+print_aanbieding()
+
